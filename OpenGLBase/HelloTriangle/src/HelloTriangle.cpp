1d9fa189d260d8f943cac617175a7cf0d192d5ad
Lix <lancer.k.lix@gmail.com>
2020-05-18 22:19:44 +0800

    finish HelloTriangle with more detail comments and a Parallelogram drawing

--- OpenGLBase/HelloTriangle/src/HelloTriangle.cpp
+++ OpenGLBase/HelloTriangle/src/HelloTriangle.cpp
@@ -39,7 +39,7 @@ int main()
 	//通知glfw使用的OpenGL版本，linux上可以使用glxinfo查询，windows下使用OpenGL Extension Viewer或者GPU Caps Viewer
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-	//通知glfw使用的OpenGL模式
+	//通知glfw使用的OpenGL模式，这里设置为核心模式
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	//通知glfw是否保留OpenGL向前兼容性，核心模式下默认不保留向前兼容，使用废弃的OpenGL函数会抛出错误中止执行
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -124,33 +124,47 @@ int main()
 	glDeleteShader(fragmentShader);
 
 	float vertices[] = {
+		//first triangle
 		-0.5f, -0.5f, 0.0f, // a left
+		-0.3f, -0.3f, 0.0f, // b left
 		 0.5f, -0.5f, 0.0f, // a right
+		 0.3f, -0.3f, 0.0f, // b right
 		 0.0f,  0.5f, 0.0f, // a top
-		-0.1f, -0.1f, 0.0f, // b left
-		 0.1f, -0.1f, 0.0f, // b right
-		 0.0f,  0.1f, 0.0f  // b top
+		 0.0f,  0.3f, 0.0f,  // b top
+		//second triangle
+		 0.0f,  0.5f, 0.0f, // a left
+		 0.0f,  0.0f, 0.0f, // b left
+		 0.5f, -0.5f, 0.0f, // a right
+		 0.0f,  0.0f, 0.0f, // b right
+		 1.0f,  0.5f, 0.0f, // a top
+		 0.0f,  0.0f, 0.0f  // b top
 	};
 
 	unsigned int VBO, VAO;
+	//分配VBO，VAO
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 
+	//绑定VBO，VAO到OPENGL上下文
 	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//开始配置VBO，VAO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	//顶点属性0由3个components组成，每个component的类型是GL_FLOAT
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float)/* 0 */, (void*)0);
+	//该顶点属性0由3个components组成，每个component的类型是GL_FLOAT，在VBO中每间隔6*sizeof(float)取一个顶点的该顶点属性
+	//stride可以理解为每2个顶点的同一个顶点属性在VBO中的间隔
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float)/* 0 */, (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//设置第2个顶点属性作为测试，如果使能，则三角形的最终顶点为两个顶点属性的坐标和，第一个顶点的该顶点属性的第一个component位于VBO起始偏移(3*sizeof(float)位置
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	//glEnableVertexAttribArray(1);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
+	//设置完VAO和VBO后，将VAO和VBO从OPENGL上下文解绑，需要时再直接绑定使用
 	glBindVertexArray(0);
+	//即使glVertexAttribPointer会记录VBO，为了安全起见，在解绑VAO后再解绑VBO，但是EBO(IBO)一定要在VAO解绑后再解绑，不然VAO会丢失EBO状态
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -163,9 +177,22 @@ int main()
 
 		glUseProgram(shaderProgram);
 
+		//再次绑定VAO可以直接获得与该VAO相关联的VBO和EBO的状态
+		//一个顶点数组对象会储存以下这些内容：
+		//1.glEnableVertexAttribArray和glDisableVertexAttribArray的调用
+		//2.通过glVertexAttribPointer设置的顶点属性配置
+		//3.通过glVertexAttribPointer调用与顶点属性关联的顶点缓冲对象(VBO)
+		//4.当目标是GL_ELEMENT_ARRAY_BUFFER的时候，VAO会储存glBindBuffer的函数调用。这也意味着它也会储存解绑调用，所以确保你没有在解绑VAO之前解绑索引数组缓冲，否则它就没有这个EBO配置了。
 		glBindVertexArray(VAO);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//从VBO中第0个顶点数据开始画3个顶点，按照三角形为图元装配，可以画出第一个三角形
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//从VBO中第3顶点数据(以顶点的整个顶点属性长度为单位)开始画3个顶点，按照三角形为图元装配，可以画出第二个三角形
+		//glDrawArrays(GL_TRIANGLES, 3, 3);
+
+		//从VBO中第0个顶点数据开始使用画6个顶点，按照三角形为图元装配，可以将前面两个三角形组成平行四边形
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		//渲染结果保存在后端缓冲，前端缓冲用于显示，交换前后端缓冲显示图像
 		glfwSwapBuffers(window);
@@ -173,9 +200,12 @@ int main()
 		glfwPollEvents();
 	}
 
+	//释放VAO
 	glDeleteVertexArrays(1, &VAO);
+	//释放VB0
 	glDeleteBuffers(1, &VBO);
 
+	//释放着色器程序对象
 	glDeleteProgram(shaderProgram);
 
 	//释放glfw窗口
